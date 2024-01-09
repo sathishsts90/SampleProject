96a19d12db3cd8ee4580cc993e555e7612465411
sathishsts90 <sathishsts90@gmail.com>
2024-01-09 13:42:58 +0530

    First time Push

---
+++ src/test/java/org/samp/TestA.java
@@ -0,0 +1,16 @@
+package org.samp;
+
+public class TestA {
+
+	private void empName() {
+System.out.println("emp name sathish");
+	}
+	private void empNo() {
+		System.out.println("emp no 12345");
+
+	}
+	private void empPhno() {
+		System.out.println("emp ph no 9944568572");
+
+	}
+}
---
+++ src/test/java/org/samp/TestB.java
@@ -0,0 +1,9 @@
+package org.samp;
+
+public class TestB {
+	
+	private void bloodgrp() {
+		System.out.println("emp bld grp is A+ve");
+	}
+
+}
