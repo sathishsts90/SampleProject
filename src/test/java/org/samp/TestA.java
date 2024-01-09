79b83fa3fd87a43d6f9c6c4f2589dae6ee99a773
sathishsts90 <sathishsts90@gmail.com>
2024-01-09 17:35:29 +0530

    modified methods in Stars

--- src/test/java/org/samp/TestA.java
+++ src/test/java/org/samp/TestA.java
@@ -3,14 +3,28 @@ package org.samp;
 public class TestA {
 
 	private void empName() {
-System.out.println("emp name sathish");
+		System.out.println("emp name sathish");
 	}
+
 	private void empNo() {
 		System.out.println("emp no 12345");
 
 	}
+
 	private void empPhno() {
 		System.out.println("emp ph no 9944568572");
 
+	}
+
+	private void jasmineFlower() {
+		System.out.println("Jasmine");
+	}
+
+	private void roseFlower() {
+		System.out.println("Rose");
+	}
+
+	private void lotusFlower() {
+		System.out.println("Lotus");
 	}
 }
